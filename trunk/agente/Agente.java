9c7a50474ea083219e05ebf30508ab810723b6d9
nicolas.cesar <nicolas.cesar@323f6821-6c2f-0410-900f-efd535441baf>
2007-05-01 18:55:32 +0000

    agregado package agente

---
+++ trunk/agente/Agente.java
@@ -0,0 +1,17 @@
+package agente;
+
+public class Agente {
+
+	public Agente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
